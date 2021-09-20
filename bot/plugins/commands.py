cba30718b0f747450422a8ee142dc5ba2d00ec7b
Muhammedrafi123 <90725957+Muhammedrafi123@users.noreply.github.com>
2021-09-21 02:10:38 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @minnaltv",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕️Click⭕️', url="https://t.me/minnaltv"
                                 )
                         ]
                     ]
